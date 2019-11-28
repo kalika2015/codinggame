29a796b4c81c688739bbc7e4207bd4bc1be3dc8c
Moussa <moussa.ly@atos.net>
2019-11-28 15:21:29 +0000

    Ajouter propriety object

--- proprietyobject.js
+++ proprietyobject.js
@@ -1,18 +1,20 @@
  function helloProperties(obj) {
- var elements = [];
-  var template = "Hello";
+	 var elements = [];
+	 var template = "Hello";
 
-    obj = {
-    john: 12,
-    brian: true,
-    doe: 'hi',
-    fred: false
- };
- 
-for (const [key, value] of Object.entries(obj)) {
-   
-     elements.push(template+"-"+key);
-}
+	    obj = {
+	    john: 12,
+	    brian: true,
+	    doe: 'hi',
+	    fred: false
+	 };
+	 
+	for (const [key, value] of Object.entries(obj)) {
+	   
+	     elements.push(template+"-"+key);
+	}
+
+	return elements;
 
 
 }
